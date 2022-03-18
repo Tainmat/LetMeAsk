aad84ee0a3009e66ffed1f81b9c5d098e7cdd0ae
TAINÃ PINO <taina.pino@mtcorp.com.br>
2022-03-17 23:02:10 -0300

    Criar Sala

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,15 +1,36 @@
-//import { useAuth } from "../hooks/useAuth";
-import { NavLink } from "react-router-dom";
+import { FormEvent, useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 
 import { Button } from "../components/Button";
+import { useAuth } from "../hooks/useAuth";
+import { database } from "../services/firebase";
 
 import "../styles/auth.scss";
 
 export function NewRoom() {
-  //const { user } = useAuth();
+  const { user } = useAuth();
+  const navigate = useNavigate();
+  const [newRoom, setNewRoom] = useState("");
+
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (newRoom.trim() === "") {
+      return;
+    }
+
+    const roomRef = database.ref("rooms");
+
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id,
+    });
+
+    navigate(`/rooms/${firebaseRoom.key}`);
+  }
 
   return (
     <div id="page-auth">
@@ -25,12 +46,17 @@ export function NewRoom() {
         <div className="mainContent">
           <img src={logoImg} alt="Letmeask" />
           <h2>Crie uma nova sala</h2>
-          <form>
-            <input type="text" placeholder="Nome da sala" />
+          <form onSubmit={handleCreateRoom}>
+            <input
+              type="text"
+              placeholder="Nome da sala"
+              onChange={(event) => setNewRoom(event.target.value)}
+              value={newRoom}
+            />
             <Button type="submit">Criar sala</Button>
             <p>
               Quer entra em uma sala existente?
-              <NavLink to="/">Clique aqui.</NavLink>
+              <NavLink to="/"> Clique aqui.</NavLink>
             </p>
           </form>
         </div>
